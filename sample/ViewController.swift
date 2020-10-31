a18020f585e8b360940673f0abbd88bc2720215e
sli7826 <stevenlin.lee@gmail.com>
2020-10-30 21:27:46 -0400

    Initial Commit

---
+++ sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sample
+//
+//  Created by Steven Li on 10/30/20.
+//  Copyright © 2020 team9. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
