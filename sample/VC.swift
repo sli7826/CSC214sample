7e36077f6704c2b001e5aa8d6e36651a91efa798
Iris Qi Miao <irism824-NF@users.noreply.github.com>
2020-11-01 10:43:26 +0800

    VC to add save data function

---
+++ sample/VC.swift
@@ -0,0 +1,17 @@
+//
+//  VC.swift
+//  sample
+//
+//  Created by QMiao on 2020/11/1.
+//  Copyright © 2020 team9. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class VC: UIViewController {
+    
+    
+    
+    
+}
